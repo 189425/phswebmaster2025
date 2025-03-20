9002bf389b840e681d748c4c4bbbb68c6d46b3a2
anon-lawrence <lawrence2411h@gmail.com>
2025-03-20 18:58:37 -0400

    Update script.js
    new

--- script.js
+++ script.js
@@ -1,57 +1,98 @@
-let subtotal = 0;
+subtotal = 0;
 
-function openTab(_, tabName) {
-	var tabcontent, tablinks;
+function addToCart(object) {
+    let container = object.parentElement.parentElement.parentElement;
 
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (let i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
+    let newDiv = document.createElement("div");
+    newDiv.style.display = "flex";
+    newDiv.style.flexDirection = "column";
+    newDiv.style.height = "50px";
+    newDiv.style.width = "100%";
+    newDiv.style.justifyContent = "center";
+    newDiv.style.border = "solid black 1px";
+    
+    let topRow = document.createElement("div");
+    topRow.style.display = "flex";
+    topRow.style.alignItems = "center";
+    topRow.style.height = "25px";
+    topRow.style.width = "100%";
+    topRow.style.gap = "20%";
 
-	tablinks = document.getElementsByClassName("tablinks");
-	for (let i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
+    let name = document.createElement("h6");
+    name.textContent = object.parentElement.childNodes[1].textContent;
+    name.style.flex = 1;
+    name.style.marginRight = "10px";
+    name.style.paddingLeft = "5px";
 
-	document.getElementById(tabName).style.display = "block";
-	_.currentTarget.className += " active";
+    topRow.appendChild(name);
+
+    // Cancel button to remove items
+    let cancelButton = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    cancelButton.className = "cancel-button";
+    cancelButton.style.width ="15px";
+    cancelButton.style.height = "15px";
+    cancelButton.style.marginRight = "5px";
+    cancelButton.onclick = function() { removeItem(this); };
+
+    let line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line1.setAttribute("x1", 0);
+    line1.setAttribute("y1", 0);
+    line1.setAttribute("x2", 15);
+    line1.setAttribute("y2", 15);
+    line1.setAttribute("stroke", "black");
+    line1.setAttribute("stroke-width", "1");
+    line1.setAttribute("stroke-linecap", "round");
+    cancelButton.appendChild(line1);
+
+    let line2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line2.setAttribute("x1", 15);
+    line2.setAttribute("y1", 0);
+    line2.setAttribute("x2", 0);
+    line2.setAttribute("y2", 15);
+    line2.setAttribute("stroke", "black");
+    line2.setAttribute("stroke-width", "1");
+    line2.setAttribute("stroke-linecap", "round");
+    cancelButton.appendChild(line2);
+
+    topRow.appendChild(cancelButton);
+    
+    let bottomRow = document.createElement("div");
+    bottomRow.style.display = "flex"; 
+    bottomRow.style.alignItems = "center";
+    bottomRow.style.justifyContent = "center"; 
+    bottomRow.style.height = "25px"; 
+    bottomRow.style.width = "100%"; 
+    let price = document.createElement("h4");
+    price.textContent = object.textContent;
+    bottomRow.appendChild(price);
+    adjustPrice("add", price.textContent.substring(1));
+
+    newDiv.appendChild(topRow);
+    newDiv.appendChild(bottomRow);
+
+    document.getElementsByClassName("cart-items")[0].appendChild(newDiv);
 }
 
-function addToCart(button) {
-    let parent = button.parentNode;
-    let clonedNode = parent.cloneNode(true);
-    clonedNode.removeChild(clonedNode.getElementsByTagName("button")[0]);
-	
-	let price = clonedNode.querySelector(".item #price").innerHTML;
-
-    let cancel = document.createElement("button");
-    cancel.innerHTML = "Remove";
-	cancel.onclick = function () {
-		price = this.parentNode.querySelector(".item #price").innerHTML;
-		this.parentNode.remove();
-		adjustPrice("subtract", price.substring(1));
-	};
-    clonedNode.appendChild(cancel);
-    
-	let pricePara = document.getElementById("total");
-    document.getElementById("Cart").insertBefore(clonedNode, pricePara);
-	adjustPrice("add", price.substring(1))
+function removeItem(object) {
+    let price = object.parentElement.parentElement.childNodes[1].textContent;
+    adjustPrice("subtract", price.substring(1));
+    object.parentElement.parentElement.remove();
 }
 
 function adjustPrice(action, price) {
-	if (action === "add") {
-		subtotal += parseFloat(price);
-	} else if (action === "subtract") {
-		subtotal -= parseFloat(price);
-	}
-	
-	subtotal = parseFloat(subtotal.toFixed(2));
-	let pricePara = document.getElementById("total");
-	pricePara.innerHTML = "Subtotal = $" + subtotal.toFixed(2);
+    if (action === "add") {
+	subtotal += parseFloat(price);
+    } else if (action === "subtract") {
+	subtotal -= parseFloat(price);
+    }
+    
+    subtotal = parseFloat(subtotal.toFixed(2));
+    let pricePara = document.getElementById("total");
+    pricePara.innerHTML = "Subtotal = $" + subtotal.toFixed(2);
 
-	let taxPara = document.getElementById("taxes");
-	taxPara.innerHTML = "Tax (8.25%) = $" + (subtotal * 0.0825).toFixed(2);
-	
-	let finalPara = document.getElementById("final");
-	finalPara.innerHTML = "Total = $" + (subtotal + (subtotal * 0.0825)).toFixed(2);
+    let taxPara = document.getElementById("taxes");
+    taxPara.innerHTML = "Tax (8.25%) = $" + (subtotal * 0.0825).toFixed(2);
+    
+    let finalPara = document.getElementById("final");
+    finalPara.innerHTML = "Total = $" + (subtotal + (subtotal * 0.0825)).toFixed(2);
 }
